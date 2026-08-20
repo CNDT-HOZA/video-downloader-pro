a5ae307c39cc924d6a52b63a4173bdb76f7faaef
Hoang <cndt-hoza@users.noreply.github.com>
2026-08-20 08:48:23 +0700

    release: v2.3.3 - Auto-update test release

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ from tkinter import filedialog
 import customtkinter as ctk
 import yt_dlp
 
-APP_VERSION = "2.3.2"
+APP_VERSION = "2.3.3"
 
 try:
     if sys.stdout and hasattr(sys.stdout, 'reconfigure'):
